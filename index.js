171bacbabfc47c65785e5c48cf20ecac026a8f97
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-06-07 11:00:45 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1=31;
+let num2=2;
+var multiply=(num1*num2);
 
-// Write your code here
+var random=(Math.floor((Math.random () +10)+1));
+
+console.log(random)
+
+let num3=9;
+let num4=5;
+var mod=(num3%num4);
+
+let max=20;
+Math.max (4,8,12,16,20)
